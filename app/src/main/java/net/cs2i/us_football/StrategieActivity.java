acec9a72bfb121d7b481d739a6e26c996f72c1da
Xydhroz <35603858+Xydhroz@users.noreply.github.com>
2018-02-19 09:24:07 +0100

    Ceci est pour la science

---
+++ app/src/main/java/net/cs2i/us_football/StrategieActivity.java
@@ -0,0 +1,20 @@
+package net.cs2i.us_football;
+
+/**Created by Xydhroz on 19/02/2018*/
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class StrategieActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strategies);
+    }
+}
