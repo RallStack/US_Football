0f0208a9cd4dd11899258c0e74103fa026fc85ac
Thomas <t.bonney77@gmail.com>
2018-02-20 16:37:53 +0100

    Création de la page Joueur Edition

---
+++ app/src/main/java/net/cs2i/us_football/JoueurEditionActivity.java
@@ -0,0 +1,17 @@
+package net.cs2i.us_football;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thomas on 20/02/2018.
+ */
+
+public class JoueurEditionActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joueur_edition);
+    }
+}
