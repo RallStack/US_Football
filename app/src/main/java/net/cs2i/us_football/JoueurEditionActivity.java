db77210f09c35c24f877f442f224f694913d3854
RallStack <duchemin.maxime.lesallieres@gmail.com>
2018-03-07 22:02:23 +0100

    Création formulaire ajoutd'un joueur (non fonctionnelle)

--- app/src/main/java/net/cs2i/us_football/JoueurEditionActivity.java
+++ app/src/main/java/net/cs2i/us_football/JoueurEditionActivity.java
@@ -1,7 +1,15 @@
 package net.cs2i.us_football;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by thomas on 20/02/2018.
@@ -9,9 +17,43 @@ import android.os.Bundle;
 
 public class JoueurEditionActivity extends Activity {
 
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    EditText edittext;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joueur_edition);
+
+        edittext = (EditText)this.findViewById(R.id.add_player_birthdate);
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(JoueurEditionActivity.this,  date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
     }
 }
