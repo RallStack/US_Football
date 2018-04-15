e63b37d8ea58f153942b18037e43259220ca2f1e
Thomas <t.bonney77@gmail.com>
2018-04-16 01:54:51 +0200

    création de l'entite Strategie

---
+++ app/src/main/java/net/cs2i/us_football/Entity/Strategie.java
@@ -0,0 +1,30 @@
+package net.cs2i.us_football.Entity;
+
+import net.cs2i.us_football.Utils.XmlHandler;
+
+/**
+ * Created by thomas on 15/04/2018.
+ */
+
+public class Strategie {
+    private static final String filename = "strategie.xml";
+    String name, image;
+    XmlHandler xmlHandler;
+
+    public Strategie(){
+        xmlHandler = new XmlHandler();
+    }
+
+
+
+        /* ------------- Getters and Setters ------------- */
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
