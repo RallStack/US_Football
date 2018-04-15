4d0ef5885a952a22162346b7ae0acde373474e0a
Thomas <t.bonney77@gmail.com>
2018-04-16 01:48:24 +0200

    redirection des pages lors de l'ajout des strategies

--- app/src/main/java/net/cs2i/us_football/NewStrategieAttaqueActivity.java
+++ app/src/main/java/net/cs2i/us_football/NewStrategieAttaqueActivity.java
@@ -4,20 +4,94 @@ package net.cs2i.us_football;
  * Created by Xydhroz on 20/02/2018.
  */
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
-public class NewStrategieAttaqueActivity extends Activity{
+import net.cs2i.us_football.Entity.Player;
+import net.cs2i.us_football.Entity.Strategie;
+import net.cs2i.us_football.Table.EquipeTable;
+import net.cs2i.us_football.Table.PlayerTable;
+import net.cs2i.us_football.Table.StrategieTable;
+import net.cs2i.us_football.Utils.RecyclerAdapter;
+import net.cs2i.us_football.Utils.VueTerrainStrategie;
 
-    private ListView playerStratListView;
+import java.util.ArrayList;
+
+public class NewStrategieAttaqueActivity extends Activity implements View.OnClickListener{
+
+//    private ListView playerStratListView;
+//
+//    private RecyclerView recyclerView;
+//    private RecyclerAdapter adapter;
+//    private ArrayList<Strategie> strategieList;
+//    private FloatingActionButton fab;
+//    private StrategieTable StrategieTable;
+
+
+    Button effacer;
+    Button ajouter;
+    //ListView strategieList;
+
+    net.cs2i.us_football.Entity.Strategie Strategie;
+
+    String tag;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation_strategies);
 
-        playerStratListView = (ListView) findViewById(R.id.player_strat_list_view);
+        ajouter = (Button) findViewById(R.id.btn_add_new_strat);
+        ajouter.setOnClickListener(this);
+
+
+    //    effacer = (Button) findViewById(R.id.btn_clear);
+    //    effacer.setOnClickListener(this);
+
+
+
+
+    //    StrategieTable = new StrategieTable(this);
+//
+    //    strategieList = new ArrayList<>();
+//
+    //    recyclerView = (RecyclerView) findViewById(R.id.recyle_view);
+    //    fab = (FloatingActionButton) findViewById(R.id.fab);
+//
+    //    recyclerView.setHasFixedSize(true);
+//
+    //    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    //    recyclerView.setLayoutManager(layoutManager);
+//
+    //    strategieList.clear();
+    //    strategieList.addAll(StrategieTable.getStrategie());
+//
+    //    //setRecyclerViewData(); //adding data to array list
+    ////    adapter = new RecyclerAdapter(this, strategieList);
+    //    recyclerView.setAdapter(adapter);
+//
+    //    fab.setOnClickListener(onAddingListener());
     }
+
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.btn_add_new_strat :
+                Intent bj = new Intent(this, StrategieActivity.class);
+                this.startActivity(bj);
+                break;
+        }
+    }
+
+
+
 
 }
--- app/src/main/java/net/cs2i/us_football/NewStrategieDefenseActivity.java
+++ app/src/main/java/net/cs2i/us_football/NewStrategieDefenseActivity.java
@@ -4,19 +4,40 @@ package net.cs2i.us_football;
  * Created by Xydhroz on 20/02/2018.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
-public class NewStrategieDefenseActivity extends Activity{
+public class NewStrategieDefenseActivity extends Activity implements View.OnClickListener{
 
     private ListView playerStratListView;
+    Button ajouter;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation_strategies);
 
-        playerStratListView = (ListView) findViewById(R.id.player_strat_list_view);
+        ajouter = (Button) findViewById(R.id.btn_add_new_strat);
+        ajouter.setOnClickListener(this);
+
+        //playerStratListView = (ListView) findViewById(R.id.player_strat_list_view);
         }
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            //case R.id.btn_clear :
+            //    VueTerrainStrategie.clear();
+            //    break;
+
+
+            case R.id.btn_add_new_strat :
+                Intent bj = new Intent(this, StrategieActivity.class);
+                this.startActivity(bj);
+                break;
+        }
+    }
 }
